9577abbe92a3e798c4c84562f8891411093b2833
Anderson Campos <158058420+AndersonCamposs@users.noreply.github.com>
2025-01-05 10:51:06 -0300

    Componentizando a linha da tabela de Cliente

---
+++ src/components/customers/CustomerRow.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const { customer } = defineProps(['customer'])
+</script>
+
+<template>
+  <CTableRow>
+    <CTableDataCell>{{ customer.id }}</CTableDataCell>
+    <CTableDataCell>{{ customer.name }}</CTableDataCell>
+    <CTableDataCell>{{ customer.cpf }}</CTableDataCell>
+    <CTableDataCell>{{ customer.email }}</CTableDataCell>
+    <CTableDataCell>{{ customer.bornDate }}</CTableDataCell>
+  </CTableRow>
+</template>
--- src/views/customers/ListCustomers.vue
+++ src/views/customers/ListCustomers.vue
@@ -1,41 +1,33 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import api from "@/services/api.js";
+import { ref, onMounted } from 'vue'
+import api from '@/services/api.js'
+import CustomerRow from '@/components/customers/CustomerRow.vue'
 
-const customerList = ref();
+const customerList = ref()
 
 onMounted(async () => {
   try {
-    const response = await api.get("/customer");
-    customerList.value = response.data;
+    const response = await api.get('/customer')
+    customerList.value = response.data
   } catch (e) {
     console.log(e.message)
   }
 })
 </script>
 
-
 <template>
   <CTable striped>
-  <CTableHead>
-    <CTableRow>
-      <CTableHeaderCell scope="col">ID</CTableHeaderCell>
-      <CTableHeaderCell scope="col">Nome</CTableHeaderCell>
-      <CTableHeaderCell scope="col">CPF</CTableHeaderCell>
-      <CTableHeaderCell scope="col">E-mail</CTableHeaderCell>
-      <CTableHeaderCell scope="col">Data de Nascimento</CTableHeaderCell>
-    </CTableRow>
-  </CTableHead>
-  <CTableBody v-for="customer in customerList" v-bind:key="customer.id">
-    <CTableRow>
-      {{ console.log(customer) }}
-      <CTableDataCell>{{ customer.id }}</CTableDataCell>
-      <CTableDataCell>{{ customer.name }}</CTableDataCell>
-      <CTableDataCell>{{ customer.cpf }}</CTableDataCell>
-      <CTableDataCell>{{ customer.email }}</CTableDataCell>
-      <CTableDataCell>{{ customer.bornDate }}</CTableDataCell>
-    </CTableRow>
-
-  </CTableBody>
-</CTable>
+    <CTableHead>
+      <CTableRow>
+        <CTableHeaderCell scope="col">ID</CTableHeaderCell>
+        <CTableHeaderCell scope="col">Nome</CTableHeaderCell>
+        <CTableHeaderCell scope="col">CPF</CTableHeaderCell>
+        <CTableHeaderCell scope="col">E-mail</CTableHeaderCell>
+        <CTableHeaderCell scope="col">Data de Nascimento</CTableHeaderCell>
+      </CTableRow>
+    </CTableHead>
+    <CTableBody v-for="customer in customerList" v-bind:key="customer.id">
+      <CustomerRow :customer="customer" />
+    </CTableBody>
+  </CTable>
 </template>
