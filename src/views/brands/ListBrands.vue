12686ddf057e8a624fd87372bdc21b09aa6509c5
Anderson Campos <anderson.camposs87@outlook.com>
2025-01-10 20:42:50 -0300

    Concluindo a funcionalidade de busca por filtro

--- src/views/brands/ListBrands.vue
+++ src/views/brands/ListBrands.vue
@@ -7,9 +7,11 @@ import { cilSearch } from '@coreui/icons';
 
 const brandList = ref([]);
 const searchValue = ref('');
+
 onMounted(async () => {
   try {
     const { data } = await api.get('/brand');
+    console.log(data);
     brandList.value = data;
   } catch (e) {
     console.log(e.message);
@@ -32,26 +34,25 @@ watch(searchValue, async (newSearchValue) => {
 </script>
 
 <template>
+  <div class="row">
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-2">
+      <CInputGroup class="mb-3">
+        <CInputGroupText id="basic-addon1"><CIcon :icon="cilSearch" /></CInputGroupText>
+        <CFormInput
+          type="text"
+          required
+          placeholder="Buscar por nome"
+          aria-label="Burcar por nome"
+          aria-describedby="basic-addon1"
+          v-model="searchValue"
+        />
+      </CInputGroup>
+    </div>
+  </div>
   <CAlert color="warning" v-if="brandList.length === 0" class="text-center">
     Não existem registros. Adicione novas marcas para que elas apareçam aqui.
   </CAlert>
-
   <div v-else>
-    <div class="row">
-      <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-2">
-        <CInputGroup class="mb-3">
-          <CInputGroupText id="basic-addon1"><CIcon :icon="cilSearch" /></CInputGroupText>
-          <CFormInput
-            type="text"
-            required
-            placeholder="Buscar por nome"
-            aria-label="Burcar por nome"
-            aria-describedby="basic-addon1"
-            v-model="searchValue"
-          />
-        </CInputGroup>
-      </div>
-    </div>
     <div class="row g-3">
       <div
         v-for="brand in brandList"
