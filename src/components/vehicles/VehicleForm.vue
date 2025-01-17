81ccaf6d63c8d8b5a709a9b483063db2762147b8
Anderson Campos <anderson.camposs87@outlook.com>
2025-01-17 09:28:56 -0300

    Melhorando o formulário do veículo

--- src/components/vehicles/VehicleForm.vue
+++ src/components/vehicles/VehicleForm.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { defineProps, defineEmits } from 'vue';
-import { cilPencil, cilListNumbered, cilAt, cilCalendar } from '@coreui/icons';
+import { defineProps, defineEmits, ref, onMounted } from 'vue';
+import { cilPencil, cilListNumbered, cilAt, cilCalendar, cilTag } from '@coreui/icons';
 import CIcon from '@coreui/icons-vue';
-import { CButton, CForm, CInputGroup } from '@coreui/vue';
+import { CButton, CForm, CFormSelect, CInputGroup } from '@coreui/vue';
+import api from '@/services/api.js';
 
 const emit = defineEmits(['showDeleteModal', 'onSubmit']);
 const props = defineProps({
@@ -11,22 +12,50 @@ const props = defineProps({
     required: true,
   },
 });
+const brandsList = ref([]);
+const categoriesList = ref([]);
+
+const loadBrandsList = async () => {
+  try {
+    const { data } = await api.get('/brand');
+    brandsList.value = data;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const loadCategoriesList = async () => {
+  try {
+    const { data } = await api.get('/category');
+    categoriesList.value = data;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+onMounted(async () => {
+  try {
+    await loadBrandsList();
+    await loadCategoriesList();
+  } catch (e) {
+    console.log(e.message);
+  }
+});
 </script>
 
 <template>
   <CForm @submit.prevent="emit('onSubmit')">
     <div class="row mb-3">
-      <div class="col-8">
+      <div class="col-4">
         <CInputGroup class="mb-3">
-          <CInputGroupText id="basic-addon1"><CIcon :icon="cilPencil" /></CInputGroupText>
-          <CFormInput
-            type="text"
-            required
-            placeholder="Nome"
-            aria-label="Nome"
-            aria-describedby="basic-addon1"
-            v-model="vehicle.name"
-          />
+          <CInputGroupText id="basic-addon1">
+            <CIcon :icon="cilTag" />
+          </CInputGroupText>
+          <CFormSelect aria-label="Select da marca do veículo">
+            <option v-for="brand in brandsList" :key="brand.id" :value="brand.id">
+              {{ brand.name }}
+            </option>
+          </CFormSelect>
         </CInputGroup>
       </div>
 
