a6ea810c1c059dd7ba78766c288efaff044619f5
Anderson Campos <anderson.camposs87@outlook.com>
2025-01-16 21:27:58 -0300

    Update VehicleListItem.vue

--- src/components/vehicles/VehicleListItem.vue
+++ src/components/vehicles/VehicleListItem.vue
@@ -3,6 +3,7 @@ import { cilCalendar, cilPencil, cilSettings, cilSpeedometer } from '@coreui/ico
 import CIcon from '@coreui/icons-vue';
 import { CCarousel, CCarouselItem } from '@coreui/vue';
 import { defineProps, defineEmits, onMounted } from 'vue';
+import numberFormatter from '@/utils/numberFormatter';
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 const props = defineProps({
@@ -11,10 +12,6 @@ const props = defineProps({
     required: true,
   },
 });
-
-const setNumberFormat = (number) => {
-  return number.toLocaleString('pt-BR');
-};
 </script>
 
 <template>
@@ -31,11 +28,11 @@ const setNumberFormat = (number) => {
     <CCardBody>
       <h5>{{ `${vehicle.brand.name} ${vehicle.model.toUpperCase()}` }}</h5>
       <p class="mt-4">
-        <strong>R$ {{ setNumberFormat(vehicle.value) }}</strong>
+        <strong>R$ {{ numberFormatter(vehicle.value) }}</strong>
       </p>
       <hr />
       <div class="d-flex justify-content-between">
-        <p><CIcon :icon="cilSpeedometer" /> {{ setNumberFormat(vehicle.odometerValue) }}</p>
+        <p><CIcon :icon="cilSpeedometer" /> {{ numberFormatter(vehicle.odometerValue) }}</p>
         <p><CIcon :icon="cilSettings" /> {{ vehicle.transmissionType }}</p>
         <p><CIcon :icon="cilCalendar" /> {{ vehicle.year }}</p>
       </div>
