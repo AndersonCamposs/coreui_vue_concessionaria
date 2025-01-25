498540359288e3310bfd9292f4d461a80390dce1
Anderson Campos <anderson.camposs87@outlook.com>
2025-01-25 13:59:20 -0300

    Criação do filtro de busca do veículo

--- src/views/vehicles/ListVehicles.vue
+++ src/views/vehicles/ListVehicles.vue
@@ -1,53 +1,91 @@
 <script setup>
-import { onMounted, ref, watch } from 'vue';
+import { onMounted, reactive, ref, watch } from 'vue';
 import api from '@/services/api';
 import VehicleListItem from '../../components/vehicles/VehicleListItem.vue';
 import CIcon from '@coreui/icons-vue';
-import { cilSearch } from '@coreui/icons';
+import { cilArrowBottom, cilArrowTop, cilSearch } from '@coreui/icons';
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCollapse,
+  CFormCheck,
+  CFormSelect,
+  CInputGroup,
+  CInputGroupText,
+} from '@coreui/vue';
+
+const filtersCollapseVisible = ref(false);
 
 const vehicleList = ref([]);
-const searchValue = ref('');
+const brandsList = ref([]);
+const searchValues = reactive({
+  brand: {
+    searchValue: '',
+    visible: false,
+  },
+});
 
 onMounted(async () => {
   try {
     const { data } = await api.get('/vehicle');
     vehicleList.value = data;
-    console.log(data);
   } catch (e) {
     console.log(e.message);
   }
 });
 
-watch(searchValue, async (newSearchValue) => {
-  try {
-    let response;
-    if (!newSearchValue) {
-      //response = await api.get('/vehicle');
-    } else {
-      //response = await api.get(`/vehicle/search?name=${encodeURIComponent(newSearchValue)}`);
+watch(
+  () => searchValues.brand.visible, // getter para a propridade reativa
+  async () => {
+    try {
+      const { data } = await api.get('/brand');
+      brandsList.value = data;
+    } catch (e) {
+      console.log(e.message);
     }
-    //vehicleList.value = response.data;
-    console.log(vehicleList.value);
-  } catch (e) {
-    console.log(e.message);
-  }
-});
+  },
+);
 </script>
 
 <template>
   <div class="row">
     <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-2">
-      <CInputGroup class="mb-3">
-        <CInputGroupText id="basic-addon1"><CIcon :icon="cilSearch" /></CInputGroupText>
-        <CFormInput
-          type="text"
-          required
-          placeholder="Buscar"
-          aria-label="Burcar"
-          aria-describedby="basic-addon1"
-          v-model="searchValue"
-        />
-      </CInputGroup>
+      <CButton color="secondary" @click="filtersCollapseVisible = !filtersCollapseVisible">
+        Filtros
+        <CIcon :icon="filtersCollapseVisible ? cilArrowTop : cilArrowBottom" />
+      </CButton>
+      <div style="min-height: 5vh">
+        <CCollapse :visible="filtersCollapseVisible" class="mt-1" style="width: 80vw">
+          <CCard>
+            <CCardBody>
+              Filtrar por:
+              <div class="row">
+                <div class="col-12 col-lg-2 col-xlg-2 col-md-4 col-sm-6">
+                  <CFormCheck label="Marca" v-model="searchValues.brand.visible" />
+                  <CFormSelect
+                    v-if="searchValues.brand.visible"
+                    v-model="searchValues.brand.searchValue"
+                  >
+                    <option v-for="brand in brandsList" :key="brand.id" :value="brand.id">
+                      {{ brand.name }}
+                    </option>
+                  </CFormSelect>
+                </div>
+                <div class="col-12 col-lg-2 col-xlg-2 col-md-4 col-sm-6">
+                  <CFormCheck label="Modelo" />
+                </div>
+                <div class="col-12 col-lg-2 col-xlg-2 col-md-4 col-sm-6">
+                  <CFormCheck label="Ano" />
+                </div>
+                <div class="col-12 col-lg-2 col-xlg-2 col-md-4 col-sm-6">
+                  <CFormCheck label="Valor" />
+                </div>
+              </div>
+            </CCardBody>
+          </CCard>
+        </CCollapse>
+      </div>
     </div>
   </div>
   <CAlert color="warning" v-if="vehicleList.length === 0" class="text-center">
