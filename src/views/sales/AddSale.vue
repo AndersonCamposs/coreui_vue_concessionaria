c542c782c64ad383de4d38bf3afb7bb815928be2
Anderson Campos <anderson.camposs87@outlook.com>
2025-02-08 21:59:45 -0300

    Melhorando a o componente de realizar venda

--- src/views/sales/AddSale.vue
+++ src/views/sales/AddSale.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { cilListNumbered } from '@coreui/icons';
+import { cilListNumbered, cilTag } from '@coreui/icons';
 import { reactive, ref } from 'vue';
+import _ from 'lodash';
 import api from '@/services/api.js';
 import cpfFormatter from '@/utils/cpfFormatter.js';
 
 const customerCpf = ref('');
+const vehiclePlate = ref('');
 
 const customer = reactive({
   id: '',
@@ -12,6 +14,31 @@ const customer = reactive({
   cpf: '',
   email: '',
   bornDate: '',
+});
+
+const vehicle = reactive({
+  id: '',
+  model: '',
+  chassisNumber: '',
+  plate: '',
+  brand: {
+    id: '',
+    name: '',
+  },
+  year: '',
+  color: '',
+  category: {
+    id: '',
+    name: '',
+    description: '',
+  },
+  transmissionType: '',
+  status: '',
+  value: '',
+  odometerValue: '',
+  description: '',
+  photos: [],
+  sale: '',
 });
 
 const fetchCustomer = async () => {
@@ -23,8 +50,21 @@ const fetchCustomer = async () => {
   }
 };
 
+const fetchVehicle = async () => {
+  try {
+    const { data } = await api.get(`/vehicle/search?plate=${vehiclePlate.value}`);
+    loadVehicleData(data);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 const loadCustomerData = (data) => {
   Object.assign(customer, data);
+};
+
+const loadVehicleData = (data) => {
+  _.merge(vehicle, data);
 };
 
 const formatCpf = (event) => {
@@ -59,5 +99,37 @@ const formatCpf = (event) => {
       </div>
     </div>
     <CAlert color="warning" class="text-center"> Busque pelo cliente para realizar a venda </CAlert>
+  </div>
+  <div v-if="customer.id">
+    <div class="row">
+      <div class="col-12">
+        <h5>
+          Qual veículo o cliente
+          <span class="text-primary">{{ customer.name }}({{ customer.cpf }})</span> irá comprar
+          hoje?
+        </h5>
+      </div>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="col-12 col-sm-3 col-md-3 col-lg-3 col-xlg-3">
+        <CInputGroup class="mb-3">
+          <CInputGroupText id="basic-addon1">
+            <CIcon :icon="cilTag" />
+          </CInputGroupText>
+          <CFormInput
+            type="text"
+            required
+            placeholder="Placa do veículo"
+            aria-label="Placa do veículo"
+            aria-describedby="basic-addon1"
+            v-model="vehiclePlate"
+          />
+        </CInputGroup>
+      </div>
+      <div class="col-12 col-sm-3 col-md-3 col-lg-3 col-xlg-3">
+        <CButton color="secondary" @click="fetchVehicle()">Buscar</CButton>
+      </div>
+    </div>
   </div>
 </template>
