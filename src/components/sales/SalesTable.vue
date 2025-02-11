7d9a51ce2d821f10d21677bb1366ab39c823d792
Anderson Campos <anderson.camposs87@outlook.com>
2025-02-10 22:16:35 -0300

    Listagem de vendas
    Criação dos componentes que irão exibir as vendas realizadas e possibilitar a exclusão das mesmas

---
+++ src/components/sales/SalesTable.vue
@@ -0,0 +1,60 @@
+<script setup>
+import {
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+  CButton,
+} from '@coreui/vue';
+import CIcon from '@coreui/icons-vue';
+import dateFormatter from '@/utils/dateFormatter';
+import numberFormatter from '@/utils/numberFormatter';
+import { cilTrash } from '@coreui/icons';
+
+const emit = defineEmits(['deleteSale']);
+
+const props = defineProps({
+  saleList: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <CAlert color="warning" v-if="saleList.length === 0" class="text-center">
+    Não existem registros. Realize vendas para que elas apareçam aqui.
+  </CAlert>
+  <div class="table-responsive" v-else>
+    <CTable striped>
+      <CTableHead>
+        <CTableRow>
+          <CTableHeaderCell>ID</CTableHeaderCell>
+          <CTableHeaderCell>CPF do Cliente</CTableHeaderCell>
+          <CTableHeaderCell>Placa do veículo</CTableHeaderCell>
+          <CTableHeaderCell>Valor</CTableHeaderCell>
+          <CTableHeaderCell>Data</CTableHeaderCell>
+          <CTableHeaderCell>Ações</CTableHeaderCell>
+        </CTableRow>
+      </CTableHead>
+      <CTableBody>
+        <CTableRow v-for="sale in saleList" :key="sale.id">
+          <CTableDataCell class="text-wrap">{{ sale.id }}</CTableDataCell>
+          <CTableDataCell class="text-wrap">{{ sale.customer.cpf }}</CTableDataCell>
+          <CTableDataCell class="text-wrap">{{ sale.vehicle.plate }}</CTableDataCell>
+          <CTableDataCell class="text-wrap">
+            R$ {{ numberFormatter(sale.vehicle.value) }}
+          </CTableDataCell>
+          <CTableDataCell class="text-wrap">{{ dateFormatter(sale.date) }}</CTableDataCell>
+          <CTableDataCell>
+            <CButton color="danger" @click="$emit('deleteSale', sale.id)">
+              <CIcon :icon="cilTrash" />
+            </CButton>
+          </CTableDataCell>
+        </CTableRow>
+      </CTableBody>
+    </CTable>
+  </div>
+</template>
--- src/views/sales/ListSales.vue
+++ src/views/sales/ListSales.vue
@@ -1,1 +1,50 @@
-<template>Lista de vendas</template>
+<script setup>
+import { onMounted, ref } from 'vue';
+import SalesTable from '@/components/sales/SalesTable.vue';
+import ConfirmDeleteModal from '@/components/ConfirmDeleteModal.vue';
+import api from '@/services/api.js';
+
+const saleList = ref([]);
+const deleteModalVisible = ref(false);
+const deletedId = ref(null);
+
+onMounted(async () => {
+  try {
+    const { data } = await api.get('/sale');
+    saleList.value = data;
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+const onDelete = async () => {
+  deleteModalVisible.value = false;
+  try {
+    const response = await api.delete(`/sale/${deletedId.value}`);
+    reloadSaleList();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const reloadSaleList = () => {
+  saleList.value = saleList.value.filter((sale) => sale.id != deletedId.value);
+};
+</script>
+
+<template>
+  <SalesTable
+    :saleList="saleList"
+    @delete-sale="
+      (id) => {
+        deletedId = id;
+        deleteModalVisible = true;
+      }
+    "
+  />
+  <ConfirmDeleteModal
+    :visible="deleteModalVisible"
+    @confirm="onDelete"
+    @close="deleteModalVisible = false"
+  />
+</template>
