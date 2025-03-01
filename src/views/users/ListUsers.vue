136d65119aadc96e5ae895cea46763e9f65c0524
Anderson Campos <anderson.camposs87@outlook.com>
2025-03-01 11:41:58 -0300

    Criando a listagem de usuários

--- src/views/users/ListUsers.vue
+++ src/views/users/ListUsers.vue
@@ -1,1 +1,20 @@
-<template>Lista de usuários</template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import UserTable from '../../components/users/UserTable.vue';
+import api from '../../services/api.js';
+
+const userList = ref([]);
+
+onMounted(async () => {
+  try {
+    const { data } = await api.get('/user');
+    userList.value = data;
+  } catch (e) {
+    console.log(e);
+  }
+});
+</script>
+
+<template>
+  <UserTable :userList="userList" />
+</template>
