eec7909d189362a9bd6351a7fbb6e3db6fbec6e9
Anderson Campos <anderson.camposs87@outlook.com>
2025-03-02 22:14:36 -0300

    Exibindo informações do perfil do usuário

---
+++ src/views/users/ProfileEdit.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { cilPencil } from '@coreui/icons';
+import CIcon from '@coreui/icons-vue';
+import { CButton, CFormInput, CInputGroup, CInputGroupText } from '@coreui/vue';
+import { reactive, ref } from 'vue';
+import api from '@/services/api.js';
+import { useAuthStore } from '@/stores/auth';
+import _ from 'lodash';
+
+const searchLogin = ref('');
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <div class="container">
+    <div class="row mb-2">
+      <h4>
+        Olá, <span class="text-info">{{ authStore.user.login }}</span
+        >. Estas são as informações de seu perfil. O que deseja fazer?
+      </h4>
+    </div>
+    <div class="row">
+      <div class="col-12 col-sm-6 col-xl-4 col-xxl-4 my-2">
+        <CWidgetStatsA color="primary" class="p-2">
+          <template #value> Informações pessoais </template>
+          <template #title>Você pode editá-las aqui.</template>
+          <template #action>
+            <CDropdown placement="bottom-end">
+              <CDropdownToggle color="transparent" class="p-0 text-white" :caret="false">
+                <CIcon icon="cil-options" class="text-white" />
+              </CDropdownToggle>
+              <CDropdownMenu>
+                <CDropdownItem href="#">Editar login</CDropdownItem>
+                <CDropdownItem href="#">Editar senha</CDropdownItem>
+              </CDropdownMenu>
+            </CDropdown>
+          </template>
+        </CWidgetStatsA>
+      </div>
+      <div class="col-12 col-sm-6 col-xl-4 col-xxl-4 my-2">
+        <CWidgetStatsA color="info" class="p-2">
+          <template #value> Suas estatísticas </template>
+          <template #title>Você pode visualizá-las aqui.</template>
+          <template #action>
+            <CDropdown placement="bottom-end">
+              <CDropdownToggle color="transparent" class="p-0 text-white" :caret="false">
+                <CIcon icon="cil-options" class="text-white" />
+              </CDropdownToggle>
+              <CDropdownMenu>
+                <CDropdownItem href="#">Minhas vendas</CDropdownItem>
+                <CDropdownItem href="#">Meus faturamentos</CDropdownItem>
+              </CDropdownMenu>
+            </CDropdown>
+          </template>
+        </CWidgetStatsA>
+      </div>
+    </div>
+  </div>
+</template>
