66cbd2d4fefc8d91ac7dc9174612997d150fce76
Anderson Campos <anderson.camposs87@outlook.com>
2025-03-09 22:15:52 -0300

    Componente para exibir as estatísticas do usuário

---
+++ src/components/users/ProfileStatistics.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { cilX } from '@coreui/icons';
+import MonthlySaleAmountChart from './MonthlySaleAmountChart.vue';
+
+const emit = defineEmits(['closeCard']);
+</script>
+
+<template>
+  <div class="container-fluid">
+    <CCard>
+      <CCardHeader>
+        <CCardTitle>
+          <CButton class="mb-1" color="danger" @click="() => $emit('closeCard')">
+            <CIcon :icon="cilX" />
+          </CButton>
+        </CCardTitle>
+        <CCardSubtitle class="mb-1"> Estatísticas pessoais </CCardSubtitle>
+      </CCardHeader>
+      <CCardBody> <MonthlySaleAmountChart /> </CCardBody>
+    </CCard>
+  </div>
+</template>
